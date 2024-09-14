db58e2c75572626ce778c1cea6bbc38c5c786854
carolpontara <pontaracarol@gmail.com>
2024-09-13 21:39:17 -0300

    initial commit

---
+++ rhout-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">
+                <img style={{ width: 246, height: 35 }} src="https://via.placeholder.com/246x35" />
+            </header>
+            <h1>Hello World</h1>
+            <p>This is a React app with a black background and white Poppins font.</p>
+        </div>
+    );
+}
+
+export default App;
